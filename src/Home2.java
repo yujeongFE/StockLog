af8069b5ee602d429a725f4c6694545211a1ebde
sunsun1188 <144991020+sunsun1188@users.noreply.github.com>
2024-01-09 21:51:50 +0900

    Update Home2.java

--- src/Home2.java
+++ src/Home2.java
@@ -2,6 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 import javax.swing.border.Border;
 
+// 패널 5
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.IOException;
+
 // 패널 5-1 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,6 +21,7 @@ import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.BorderFactory;
+
 
 
 // 패널 1에 대한 동작을 처리하는 클래스
